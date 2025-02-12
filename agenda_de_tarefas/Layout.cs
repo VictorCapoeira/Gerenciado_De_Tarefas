06bf13ce03b9dc1932fc84cef079a69d51db5294
Victor <victorgit28@outlook.com>
2025-02-12 09:00:01 -0300

    Criado metodo para Exibir Cabecalho

--- agenda_de_tarefas/Layout.cs
+++ agenda_de_tarefas/Layout.cs
@@ -1,4 +1,11 @@
+using Layout_console;
 namespace Layout
 {
-    
+    public class LayoutGerenciador{
+        public static void ExibirCabecalho(string titulo){
+            LayoutPosicao.Centralizar($"╔{new string('═', titulo.Length + 2)}╗");
+            LayoutPosicao.Centralizar($"║{new string(' ', 1)}{titulo}{new string(' ', 1)}║");
+            LayoutPosicao.Centralizar($"╚{new string('═', titulo.Length + 2)}╝");
+        }
+    }
 }
--- agenda_de_tarefas/Program.cs
+++ agenda_de_tarefas/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Agenda;
+using Layout;
+LayoutGerenciador.ExibirCabecalho("GERENCIADO DE TAREFAS KKKK");
