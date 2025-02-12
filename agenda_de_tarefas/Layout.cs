1050cb2bfc71ad0dd4e9b9bf84464ca00fb135d9
email <email>
2025-02-12 11:13:23 -0300

    Minor change

--- agenda_de_tarefas/Layout.cs
+++ agenda_de_tarefas/Layout.cs
@@ -5,23 +5,22 @@ namespace Layout
     {
         public static void ExibirCabecalho(string titulo, Boolean estado = false)
         {
-            if (estado)
-            {
-
                 int largura = Console.WindowWidth;
-
-                // Criando o cabeçalho com bordas
+                char[] letraMais = titulo.ToCharArray();
+                letraMais[0] = char.ToUpper(letraMais[0]);
+                string tituloMais = new string(letraMais);
+                
                 string linhaSuperior = $"╔{new string('═', titulo.Length + 2)}╗";
-                string linhaMeio = $"║ {titulo.ToUpper()} ║";
+                string linhaMeio = $"║ {(estado ? titulo.ToUpper() : tituloMais )} ║";
                 string linhaInferior = $"╚{new string('═', titulo.Length + 2)}╝";
 
-                // Calcula a posição X (centralizado horizontalmente)
+                
                 int posX = (largura - linhaSuperior.Length) / 2;
 
-                // Obtém a posição atual do cursor para centralizar na mesma linha
+                
                 int posY = Console.CursorTop;
 
-                // Posiciona e escreve no console
+                
                 Console.SetCursorPosition(posX, posY);
                 Console.WriteLine(linhaSuperior);
 
@@ -30,17 +29,6 @@ namespace Layout
 
                 Console.SetCursorPosition(posX, posY + 2);
                 Console.WriteLine(linhaInferior);
-            }
-            else
-            {
-                char[] letraMais = titulo.ToCharArray();
-                letraMais[0] = char.ToUpper(letraMais[0]);
-                string tituloMais = new string(letraMais);
-                Centralizar($"╔{new string('═', titulo.Length + 2)}╗");
-                Centralizar($"║{new string(' ', 1)}{tituloMais}{new string(' ', 1)}║");
-                Centralizar($"╚{new string('═', titulo.Length + 2)}╝");
-            }
-
         }
     }
 }
