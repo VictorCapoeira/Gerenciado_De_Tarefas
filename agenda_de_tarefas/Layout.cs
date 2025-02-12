6ef5227f6dea06dca0800957a9171e19faa8566d
Victor <victorgit28@outlook.com>
2025-02-12 10:57:16 -0300

    Criado metodo para centralizar textos

--- agenda_de_tarefas/Layout.cs
+++ agenda_de_tarefas/Layout.cs
@@ -2,8 +2,14 @@ using Layout_console;
 namespace Layout
 {
     public class LayoutGerenciador{
+
+        public static void Centralizar(string texto){
+            int largura = Console.WindowWidth;
+            int altura = Console.WindowHeight;
+        }
         public static void ExibirCabecalho(string titulo, Boolean estado = false){
             if(estado){
+
                 LayoutPosicao.Centralizar($"╔{new string('═', titulo.Length + 2)}╗");
                 LayoutPosicao.Centralizar($"║{new string(' ', 1)}{titulo.ToUpper()}{new string(' ', 1)}║");
                 LayoutPosicao.Centralizar($"╚{new string('═', titulo.Length + 2)}╝");
